5187582803aa8a7e220d20ef8d04bde99a4faa72
abngal <33404820+abngal@users.noreply.github.com>
2020-03-23 23:34:31 +0800

    Update .rc.sh

--- .rc.sh
+++ .rc.sh
@@ -10,4 +10,4 @@ wget "$FILE" https://raw.githubusercontent.com/abngal/rc/master/.abn.rc
 grep -qF -- "$LINE" "$RC1" || echo "$LINE" >> "$RC1"
 grep -qF -- "$LINE" "$RC2" || echo "$LINE" >> "$RC2"
 
-SOURCE "$FILE"
+source "$FILE"
