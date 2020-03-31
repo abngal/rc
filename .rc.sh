2edf7bf3d5c351e8c2d75a6c952e6457a56ea898
abngal <33404820+abngal@users.noreply.github.com>
2020-04-01 02:58:53 +0800

    Update .rc.sh

--- .rc.sh
+++ .rc.sh
@@ -7,7 +7,7 @@ FILE='~/.abn.rc'
 LINE="source $FILE"
 
 rm -f $FILE
-wget "$FILE" https://raw.githubusercontent.com/abngal/rc/master/.abn.rc
+wget -O "$FILE" https://raw.githubusercontent.com/abngal/rc/master/.abn.rc
 grep -qF -- "$LINE" ~/.bashrc || echo "$LINE" >> ~/.bashrc
 grep -qF -- "$LINE" ~/.zshrc || echo "$LINE" >> ~/.zsshrc
 
